2c36f7ef6fc0611175097855f22657a7f75e7808
mofadl237 <mofadl237@gmail.com>
2025-06-05 04:11:01 +0300

    Add Api For Submit Data

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
-ECHO is on.
+//All Variable
+const data = {
+  farmName: "",
+  phoneNumber: "",
+  email: "",
+  cowCount: 89,
+};
+let email = document.querySelector('[type="email"]');
+let farmName = document.getElementById("farmName");
+let phoneNumber = document.getElementById("phoneNumber");
+const submitDataContact = document.getElementById("submitDataContact");
+
+//2- Handler Function Logic
+const handlerDataSubmit = (e) => {
+  e.preventDefault();
+  data.email = email.value;
+  data.farmName = farmName.value;
+  data.phoneNumber = phoneNumber.value;
+  console.log(data);
+
+  const myHttp = new XMLHttpRequest();
+  // myHttp.open("POST", "https://cors-anywhere.herokuapp.com/http://farmxpertapi.runasp.net/api/ClientRequest/Submit");
+  myHttp.open(
+    "POST",
+    "http://farmxpertapi.runasp.net/api/ClientRequest/Submit"
+  );
+
+  myHttp.setRequestHeader("Content-Type", "application/json");
+
+  myHttp.onreadystatechange = function () {
+    if (myHttp.readyState === 4) {
+      if (myHttp.status === 200) {
+        console.log("Done ✅", myHttp.responseText);
+      } else {
+        console.error("Error ❌", myHttp.status, myHttp.responseText);
+      }
+    }
+  };
+
+  myHttp.send(JSON.stringify(data));
+};
+
+//3-Add Function To Buttons && Run
+
+submitDataContact.addEventListener("click", handlerDataSubmit);
