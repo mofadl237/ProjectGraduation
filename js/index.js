231ea939aa8c347bbb0613cb4dc4726bfa89747e
mofadl237 <mofadl237@gmail.com>
2025-06-05 04:28:44 +0300

    Last Update Mix Context

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ const handlerDataSubmit = (e) => {
   console.log(data);
 
   const myHttp = new XMLHttpRequest();
-  myHttp.open("POST", "https://cors-anywhere.herokuapp.com/https://farmxpertapi.runasp.net/api/ClientRequest/Submit");
+   myHttp.open("POST", "https://cors-anywhere.herokuapp.com/http://farmxpertapi.runasp.net/api/ClientRequest/Submit");
 //   myHttp.open(
 //     "POST",
 //     "http://farmxpertapi.runasp.net/api/ClientRequest/Submit"
